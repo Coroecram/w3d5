1ebd90713c92b99693763b42bd7d89c6ca66ecd6
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-16 11:30:16 -0400

    Complete attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,14 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |variable_name|
+      define_method(variable_name) do
+        instance_variable_get("@#{variable_name}")
+      end
+      variable_set = "#{variable_name}=".to_sym
+      define_method(variable_set) do |value|
+        instance_variable_set("@#{variable_name}", value)
+      end
+    end
   end
 end
